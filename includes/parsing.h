767174aaef5f80ff4ff1bb12def414926e6d2225
Louis GUILLAUME <lguillau@e2r5p20.clusters.42paris.fr>
2022-01-07 18:35:12 +0100

    nrm

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 18:11:42 by lguillau          #+#    #+#             */
-/*   Updated: 2022/01/07 18:28:52 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/01/07 18:34:51 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@
 /* Tools Functs */
 
 char	**ft_split(char const *s, char c);
-int	check_for_alpha(char *s);
+int		check_for_alpha(char *s);
 
 #endif
--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,12 +6,12 @@
 /*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 18:19:44 by lguillau          #+#    #+#             */
-/*   Updated: 2022/01/07 18:13:36 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/01/07 18:34:26 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAPP_H
-# define PUSH_SWAPP_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 # include <unistd.h>
 # include <stdlib.h>
