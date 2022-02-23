4dd2148eda059241e777dc922c94ecd0af695669
Louis GUILLAUME <lguillau@e2r5p20.clusters.42paris.fr>
2022-02-23 12:44:17 +0100

    lis test

--- srcs/sorting/lsi.c
+++ srcs/sorting/lsi.c
@@ -6,11 +6,11 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 16:15:01 by lguillau          #+#    #+#             */
-/*   Updated: 2022/02/16 16:49:23 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/02/23 12:42:22 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../push_swap.h"
+#include "../../includes/push_swap.h"
 
 void	ra_no_print(int *tab, int len)
 {
@@ -40,12 +40,67 @@ void	rra_no_print(int *tab, int len)
 	tab[0] = tmp;
 }
 
-int	find_lis(int *tmp, int len)
+int	*fill_lis(int *lis, int l_len, int save_pos, int *tmp)
 {
-	int	*lis;
-	int	l_len;
+	int	i;
 
-	w
+	i = save_pos;
+	while (--l_len >= 0)
+	{
+		lis[l_len] = tmp[save_pos];
+		while 
+	}
+}
+
+int	try_lis(int *tmp, int len)
+{
+	int	l_len;
+	int	nb;
+
+	l_len = 1;
+	nb = tmp[len];
+	while (--len >= 0)
+	{
+		if (tmp[len] < nb)
+		{
+			l_len++;
+			nb = tmp[len];
+		}
+	}
+	return (l_len);
+}
+
+int	*find_lis(int *tmp, int len)
+{
+	int	l_len;
+	int	save_pos;
+	int	res;
+	int	*lis;
+
+	l_len = 0;
+	save_pos = len;
+	while (len-- > 0)
+	{
+		res = try_lis(tmp, len);
+		if (l_len < res)
+		{
+			l_len = res;
+			save_pos = len;
+		}
+	}
+	lis = malloc(sizeof(int) * (l_len + 1));
+	if (!lis)
+		return (0);
+	//save_pos bonne case -- l_len len total
+	lis = fill_lis(lis, l_len, save_pos, tmp);
+	return (lis);
+}
+
+int	main(void)
+{
+	int	tmp[10] = {0, 4, 6,1,2,10,7,3,8,9};
+	int 	len = 10;
+	find_lis(tmp, len);
 }
 
 void	ft_sort(t_stack *s)
@@ -66,7 +121,7 @@ void	ft_sort(t_stack *s)
 	i = -1;
 	nb = tmp[0];
 	index = 0;
-	while (++i s->len_a)
+	while (++i < s->len_a)
 	{
 		if (tmp[i] < nb)
 		{
