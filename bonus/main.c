4eff06e4a3012134222d04a98ddf9b6ed63c60e6
LinHome <louisguillaume000@gmail.com>
2022-02-27 22:08:21 +0100

    toto

---
+++ bonus/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 18:52:45 by lguillau          #+#    #+#             */
+/*   Updated: 2022/02/27 19:05:49 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bonus.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack	*stacks;
+	int		ret;
+
+	if (argc < 2)
+		exit(1);
+	stacks = malloc(sizeof(struct s_list) * 1);
+	if (!stacks)
+		exit(1);
+	ret = ft_parse(argv, stacks);
+	if (ret == 0)
+	{
+		free(stacks);
+		exit(1);
+	}
+	//checker(stacks);
+	ft_free_stacks(stacks);
+	return (0);
+}
