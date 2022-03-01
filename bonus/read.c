85f78c60ba27bcf9bf244de4741a5c745d290414
Louis GUILLAUME <lguillau@e2r5p20.clusters.42paris.fr>
2022-03-01 12:03:19 +0100

    read

--- bonus/read.c
+++ bonus/read.c
@@ -6,13 +6,13 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:23:17 by lguillau          #+#    #+#             */
-/*   Updated: 2022/03/01 00:27:48 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/03/01 12:01:47 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bonus.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+static int	ft_strcmp(const char *s1, const char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
@@ -22,11 +22,39 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 
+static void	quit_clean(char *str, t_stack *s)
+{
+	ft_free_stacks(s);
+	free(str);
+	ft_error();
+}
+
 static void	call_op(t_stack *s, char *str)
 {
-	(void)s;
-	if (ft_strcmp(str, "pa") == 0)
-		printf("toto");
+	if (ft_strcmp(str, "pa\n") == 0)
+		pa_np(s);
+	else if (ft_strcmp(str, "pb\n") == 0)
+		pb_np(s);
+	else if (ft_strcmp(str, "sa\n") == 0)
+		sa_np(s);
+	else if (ft_strcmp(str, "sb\n") == 0)
+		sb_np(s);
+	else if (ft_strcmp(str, "ss\n") == 0)
+		ss_np(s);
+	else if (ft_strcmp(str, "ra\n") == 0)
+		ra_np(s);
+	else if (ft_strcmp(str, "rb\n") == 0)
+		rb_np(s);
+	else if (ft_strcmp(str, "rr\n") == 0)
+		rr_np(s);
+	else if (ft_strcmp(str, "rra\n") == 0)
+		rra_np(s);
+	else if (ft_strcmp(str, "rrb\n") == 0)
+		rrb_np(s);
+	else if (ft_strcmp(str, "rrr\n") == 0)
+		rrr_np(s);
+	else
+		quit_clean(str, s);
 }
 
 void	checker(t_stack *s)
@@ -41,4 +69,8 @@ void	checker(t_stack *s)
 		str = get_next_line(0);
 	}
 	free(str);
+	if (ft_is_it_sorted(s->stack_a, s->len_a) && s->len_b == 0)
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
 }
